950ca63c7823329be51eba72654311fbe5e6ed69
Arnav Sastry <arnavsastry@gmail.com>
2020-12-02 00:33:54 -0600

    Clean up Day 1 art

--- day01/day01.c
+++ day01/day01.c
@@ -2,21 +2,18 @@
 #include  <stdlib.h>
 
 // AOC DAY NUMBER //
-const int M=2020,D=9
-;char b[D],h[M];main
-(int c,char **v){int
-x,i,j,a=0;  /**/FILE
-*f=fopen    ("01.in"
-,"r");      ;;while(
-fgets(b,D   ,f)){;x=
-atoi(b);;   if(x<M){
-h[x]=1;}}   for(i=0;
-i<M;++i){   for(j=i;
-j<M;++j){   if (h[i]
-&&h[j] &&   i+j<=M){
-if(c==1){   if(i+j==
-M)a=i*j;}   else if(
-h[M-             i-j
-])a=             i*j
-*(M-i-j);}}}fclose(f
-);printf("%d\n",a);}
+/**/const int M=2020
+;char h[M];int main(
+int c,char  **v){int
+x,i,j,a=    0;while(
+scanf       (" %d",&
+x)>0)if(    x<M){h[x
+]=1;}for    (i=0;i<M
+; ++i) {    for(j=i;
+j<M;++j)    {if(h[i]
+&&h[j]&&    i+j<=M){
+if(c==1)    {if (i+j
+==M)a=i     *j;}else
+if(h            [M-i
+-j])a=i*j*(M-i-j);}}
+}printf("%d\n",a);;}
