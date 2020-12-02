9301023dd4e0429a3ca3e5e6e20f322e1c577366
Arnav Sastry <arnavsastry@gmail.com>
2020-12-02 16:55:52 -0600

    Fix shape of 2

--- day02/day02.c
+++ day02/day02.c
@@ -4,21 +4,20 @@
 
 // AOC DAY NUMBER //
 char*s=" %s%s%s";int
-main(int argc,char**
-argv){char c[99],a[9
-],b[9          ];int
-y=0             ,z=0
-;    while(    scanf
-(s,a,b,c)>    0){int
-u, h,x=+    strcspn(
-a,"-");    a[x]='\0'
-;u =+     atoi(a);h=
-atoi(    a+x+1); int
-f=0;    for(char*k=c
-;*k    ;++k){f+=*k==
-*b     ;}y+=u<=+f&&f
-<=               h;z
-+=(             c[--
-u]==*b)^(c[--h]==*b)
-;}{}/*DAY 2*/printf(
-"%d\n",argc-1?z:y);}
+main(int g,char**v){
+char c[99],a[9],b[9]
+,*k;/**/int y=0,z=0;
+while(       scanf(s
+,a,b           ,c)>0
+){     int u,    h,x
+=strcspn(a,"-"    );
+;a[x]=0;u=atoi    (a
+);h=atoi(a+     x+1)
+;int f=0;     for (k
+=c;*k;     ++k){f+=*
+k==      *b;}y+=u<=f
+&&f              <=h
+;z              +=(c
+[--u]/*DAY2*/==*b)^(
+c[--h]==*b);}printf(
+"%d\n", --g?+z:+y);}
