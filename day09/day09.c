13bffab13aadf1de4ddeac45f542a82f87b41172
Arnav Sastry <arnavsastry@gmail.com>
2020-12-09 01:37:39 -0600

    Can't sleep; better 9

--- day09/day09.c
+++ day09/day09.c
@@ -7,20 +7,21 @@ c,char**IX){for(p=a;
 scanf(        "%ld",
 p++             )>0;
 ++     n);for   (v=1
-,p    =a+25;v    &&p
-<a    +n;++p)   for(
-v=0,   q=p-25;   q<p
-;++q    )for(r  =q+1
-;r<p;r          ++)v
-|=*q+*r==*p;    long
-y=+*--p;for(    p=a;
-!z&&p<a+n;++    p){ 
-for(v=0,q=p;    q<a+
-n&&v<y;v+=*q    ++);
-if(v==y)for(    n=0,
+,p    =a+25;v   &&p<
+a+    +n;++p)   for(
+q=    p-25,v=   0;q<
+p;    ++q)for   (r=q
++1    ;r<p;r    ++)v
+|=    *q+*r==   *p;;
+long            y=+*
+--p;for(p=a;    !z&&
+p<a+n;++p){     for(
+v=0,q=p;q<a+    n&&v
+<y;v+=*q++);    if(v
+==    y)for(    n=0,
 z=   v,r=p;r    <q;*
 r>   n?n=*r:    9,*r
 <z    ?z=*     r:9,r
 ++);          }z+=n;
-999,9999,999;printf(
+int nine=0x9;printf(
 "%ld\n",--*&c?z:y);}
