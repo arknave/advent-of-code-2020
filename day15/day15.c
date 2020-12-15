b872801465123e357dce34cd1c09d98ead08ece2
Arnav Sastry <arnavsastry@gmail.com>
2020-12-15 00:13:31 -0600

    Format Day 15 in C

--- day15/day15.c
+++ day15/day15.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
+#include/*PRESS*/<stdio.h>
 
-int a[45151515],n,x,v,m;
-int main(int c, char**g){
-    for(m=--c?3e7:2020;scanf("%d%*c",&x)>0;a[x]=++n);
-    for(a[x]=0;n<m;v=a[x]?n-a[x]:0,a[x]=n++,x=v);
-    printf("%d\n",v);
-}
+            int a[1<<5
+         *5],n,x,v,m;int
+         main(      int c
+       ,char*        *g){
+       for(m
+       =--c?
+       3.e7:
+       2020;
+  scanf("%d%*c",&*&x
+ )>0;a[x]=++n);for(
+       a[x]=0
+       ;n<m;v
+       =a[x]?
+       n-a[x]
+       :0,a[x
+       ]=n++,
+       x=+v);
+      printf(""
+    "%d\n",v|0);}
+
+/*** TO PAY RESPECTS ***/
