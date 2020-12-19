924b4a4af89f58b7e9fc349dc0481d85e3d2a0b1
Arnav Sastry <arnavsastry@gmail.com>
2020-12-19 00:29:44 -0600

    Add day 18 in C

---
+++ day18/day18.c
@@ -0,0 +1,19 @@
+#include/*math=math*/<stdio.h>
+
+// AOC 2020 DAY 18  SYMBOLS //
+char*p,*q,b[324];long f,z,d(),
+e();;    long x(){long(*g)()=f
+?e:d,    a=g(),b,i;for(;*p==+1
+||(!f    &&*p==2);b=g(),i?a+=+
++            b:(a*=b))i=*p++==
++            2;return +a;}long
+d(){{    long a=7<=*p&&*p<=16?
+*p-7:    (++p,+x());++p;return
++a;}}    long e    (){    long
+a=d();for(;*p==2    ;    a+=d(
+))++p;return a;/*       */}int
+main(int c,char**g     ){for(f
+=--c>0;q=gets(b);       *p=0,p
+=b,z+=x())for(p=    b    ;*q;q
+++)*q^32?*p++=(    *q)    -4*9
+-5:0;printf("%ld\n",z);/*18*/}
