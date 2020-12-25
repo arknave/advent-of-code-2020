2eca3b9517f0d4b6ce56c9b1560efdbcd41eb0dd
Arnav Sastry <arnavsastry@gmail.com>
2020-12-25 15:18:50 -0600

    Improve day 25 art

--- day25/day25.c
+++ day25/day25.c
@@ -1,20 +1,20 @@
 #include   <stdio.h>
 
-        //
-       long
-      x,y,M=
-     20201227
-    ,v=1,a,r=1
-   ;int main(){
-  scanf(" %ld%ld"
- ,&x,&y);for(;v^x;
- ++a)v=7L*v%M;for(;
-a;y=y*y%M,a/=2)a&1?r
-       =r*y%M
-       :25;//
-       printf
-       ("%ld"
-       "\n",r
-       );25;}
+         //
+        long
+       x,y,M=
+      20201227
+     ,v=1,a,r=1
+    ;int main(){
+   scanf(" %ld%ld"
+  ,&x,&y);for(;v^x;
+ ++a)v=7L*v%M;for(;a
+ ;y=y*y%M,a/=2)a&1?r
+        =r*y%M
+        :+5*5;
+        printf
+        ("%ld"
+        "\n",r
+        );25;}
 
 // AOC 2020 DAY 25//
