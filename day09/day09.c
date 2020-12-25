114a92f236606bd7d4288306aa90e29f1cfb3648
Arnav Sastry <arnavsastry@gmail.com>
2020-12-25 15:24:13 -0600

    More tweaks to day 9 c art

--- day09/day09.c
+++ day09/day09.c
@@ -4,16 +4,16 @@
 long*p,*q,*r,a[9999]
 ,n,v,z;;int main(int
 c,char**IX){for(p=a;
-scanf(        "%ld",
+scanf        ("%ld",
 p++             )>0;
-++    n);for    (v=1
+++   n);for     (v=1
 ,p   =a+25;v    &&p<
 a+   +n;++p)    for(
 q=   p-25,v=    0;q<
 p;   ++q)for    (r=q
-+1   ;r<p;r     ++)v
-|=*   q+*r      ==*p
-;long      y    =*--
++1    ;r<p;r    ++)v
+|=*             q+*r
+==*p;long y=    +*--
 p;for(p=a;!z    &&p<
 a+n;++p,011)    {for
 (v=0,q=p;q<a    +n&&
